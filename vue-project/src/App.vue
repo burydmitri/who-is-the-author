91f9417a9394911ba6a124e281b60d179afb3a66
Dmitry Bury <burydima28122003@gmail.com>
2024-08-08 12:40:21 +0300

    chore: clear and add radix-ui and less

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,26 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
+  <main class="main">
+    <h1 class="main__title">Hello, World!</h1>
   </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
+<script >
+</script>
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
+<style lang="less" scoped>
+  .main {
+    height: 100vh;
+    width: 100%;
 
-@media (min-width: 1024px) {
-  header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    align-items: center;
+    justify-content: center;
+  
+    &__title {
+      font-size: 52px;
+
+      color: var(--primary-color)
+    }
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
